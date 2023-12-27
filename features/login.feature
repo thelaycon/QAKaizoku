c9e90e61f2547129334206b95d2d98e7445da128
thelaycon <tobitobitobiwhy@gmail.com>
2023-12-27 10:13:09 +0100

    login tests

--- features/login.feature
+++
@@ -1,6 +0,0 @@
-Feature: Scraper logins to Linkedin account without detection
-
-	Scenario: User logs in
-		Given: the homepage is opened
-		When: the login form is filled and clicked
-		Then: user should be logged in successfully
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,22 +1,28 @@
+import pytest
 from DrissionPage import ChromiumPage
 from pytest_bdd import parsers, scenario, given, when, then
 
-from login import loginAccount
+from pages.homepage import homepage
+from pages.login import loginAccount
 
-@scenario("features/login.feature", "Scraper logins to Linkedin account without detection")
-def test_linkedIn_login():
-    pass
+@pytest.fixture
+def page():
+    return ChromiumPage()
+
+@scenario("features/login.feature", "User logs in")
+def test_linkedIn_login(page):
+    page.clear_cache()
+    page.quit()
     
-    
-@given("the homepage is opened", target_fixture="homepage"):
-def homepage():
-    p = ChromiumPage()
-    loginAccount(p)
+@given("the homepage is opened", target_fixture="home")
+def home(page):
+    return homepage(page)
     
 @when("the login form is filled and clicked")
- def login():
-    pass
+def login(home):
+    loginAccount(home)
 
-@then("user should be logged in successfully"):
-def atHome():
-    pass
+@then("user should be logged in successfully")
+def atHome(home):
+    home.wait.load_start()
+    assert "https://www.linkedin.com/feed/" in home.url
